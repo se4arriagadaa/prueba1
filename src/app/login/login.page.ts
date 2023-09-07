fbdfb8d2721d3f5752d7d8e75ef2b3037723c177
nixtoon <127158636+nixtoon@users.noreply.github.com>
2023-09-07 00:13:36 -0300

    login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  usuario: string = '';
+  pass: string = '';
+  mensaje: string = '';
 
-  constructor() { }
+  validacion1() {
+    if (this.usuario.length < 3) {
+      this.mensaje = 'Debes tener entre 3 y 8 caracteres';
+    } else {
+      this.mensaje = '';
+    }
+  }
 
-  ngOnInit() {
+  validacion2() {
+    if (this.pass.length < 3) {
+      this.mensaje = 'Deben ser 4 caracteres';
+    } else {
+      this.mensaje = '';
+    }
+  }
+
+  bloquearBtn(): boolean {
+    return this.usuario.length < 3 || this.pass.length != 4;
+  }
+
+
+
+  constructor(private navCtrl: NavController) { }
+  Ingresar() {
+    this.navCtrl.navigateForward('/home', {
+      queryParams: {
+        value: this.usuario,
+      },
+    });
   }
 
 }
