007ca9d2f09610d592411b42d56d2fa6d8c767c7
se4arriagadaa <84482238+se4arriagadaa@users.noreply.github.com>
2023-09-06 23:55:56 -0300

    update recovery

--- src/app/recovery/recovery.page.ts
+++ src/app/recovery/recovery.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recovery',
@@ -6,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recovery.page.scss'],
 })
 export class RecoveryPage implements OnInit {
+  email: string = '';
+  constructor(private toastController: ToastController) { }
 
-  constructor() { }
+  enviarSolicitud() {
+    if(this.validarEmail(this.email)) {
+    // mensaje de exito
+    this.mostrarMensaje('Solicitud enviada con éxito');
 
+    // clear input
+    this.email = '';
+  } else {
+    this.mostrarMensaje('Ingrese un correo valido');
+  }
+}
+  async mostrarMensaje(mensaje: string) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 2000, // duracion de mensaje
+      position: 'top', // posicion de mensaje
+    });
+    toast.present();
+  }
+
+  validarEmail(email: string): boolean { //validar formato correo
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+  }
+  
   ngOnInit() {
   }
 
