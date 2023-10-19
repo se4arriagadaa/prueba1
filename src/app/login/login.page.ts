793283cbf377dd8d58ddcfac62c8ef6edc9e12fc
se4arriagadaa <84482238+se4arriagadaa@users.noreply.github.com>
2023-10-19 20:37:08 -0300

    login update 3

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -13,70 +13,60 @@ export class LoginPage implements OnInit {
 
   hide = true;
   private isLoggedIn: boolean = false;
-  usuario = { texto: ''};
-  password = { texto: ''};
+  usuario = { texto: '' };
+  password = { texto: '' };
+
 
   mostrarMensaje: boolean = false;
 
   constructor(private apiService: ApiService, private router: Router) { }
-  
+
   login() {
     this.apiService.buscarUsuario(this.usuario.texto, this.password.texto).subscribe(
-    (response) => {
-      if (this.verificarCredenciales(response)) {
-        const navigationExtras: NavigationExtras = {
-          state: {
-            mensaje: '¡Bienvenido! Has iniciado sesion correctamente.'
-          }
+      (response) => {
+        console.log(response);
+        if (response) {
+          this.IrAlHome()
         };
-        this.router.navigate(['/home'], navigationExtras);
-      } else {
-        console.log('Nombre o contraseña incorrecto.')
-      }
-    },
-    (error) => {
-      console.error('Error en la solicitud:', error);
+});
+  }
+
+verificarCredenciales(response: any): boolean {
+  return response.User === this.usuario.texto && response.password === this.password.texto;
+}
+
+bloquearBtn(): boolean {
+  return this.usuario.texto.length < 3 || this.password.texto.length != 4;
+}
+
+IrAlHome() {
+  let navigationExtras: NavigationExtras = {
+    state: {
+      nombreUsuario: this.usuario.texto
     }
-    );
   }
+  this.router.navigate(['/home'], navigationExtras);
+}
 
-  verificarCredenciales(response: any): boolean {
-    return response.User === this.usuario.texto && response.password === this.password.texto;
-    return true; 
-  }
+recovery() {
+  this.router.navigate(['/recovery'])
+}
 
-  bloquearBtn(): boolean {
-    return this.usuario.texto.length < 3 || this.password.texto.length != 4;
+validarUsername(user: string): string | null {
+  if (user.length < 3 || user.length > 8) {
+    return 'El nombre de usuario debe tener entre 3 y 8 caracteres.';
   }
+  return null;
+}
 
-  IrAlHome() {
-    let navigationExtras: NavigationExtras = {
-      state: {
-        nombreUsuario: this.usuario.texto
-      }
-    }
-    this.router.navigate(['/home'], navigationExtras);
+validarPassword(pass: string): string | null {
+  if (pass.length !== 4) {
+    return 'La contraseña debe tener 4 caracteres.';
   }
+  return null;
+}
 
-  recovery() {
-    this.router.navigate(['/recovery'])
-  }
-
-  validarUsername(user: string): string | null {
-    if (user.length < 3 || user.length > 8) {
-      return 'El nombre de usuario debe tener entre 3 y 8 caracteres.';
-    }
-    return null;
-  }
-
-  validarPassword(pass: string): string | null {
-    if (pass.length !== 4) {
-      return 'La contraseña debe tener 4 caracteres.';
-    }
-    return null;
-  }
-
-  ngOnInit() {
-  }
+ngOnInit() {
+}
 
 }
